912389cbc1536aad873c19311f9e11fdfa1d184c
Mohak-Trivedi <trivedimohak@gmail.com>
2024-11-05 12:43:37 +0530

    completed basic version

---
+++ file-explorer/src/components/Folder.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function Folder({ explorer }) {
+  console.log(explorer);
+  const [expand, setExpand] = useState(false);
+
+  if (explorer.isFolder) {
+    return (
+      <div style={{ marginTop: 5 }}>
+        <div
+          className="folder"
+          onClick={() => {
+            setExpand(!expand);
+          }}
+        >
+          <span>{explorer.name}</span>
+        </div>
+
+        <div style={{ display: expand ? "block" : "none" }}>
+          {explorer.items.map((exp) => {
+            return <Folder explorer={exp} key={exp.id} />;
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    return <span className="file">{explorer.name}</span>;
+  }
+}
+
+export default Folder;
